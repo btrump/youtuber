5dc1a677ba4a3298142ddf2589dcd821d84530f9
Blair Trump <blair@blairtrump.com>
2012-08-03 23:05:18 -0700

    .

--- admin.py
+++ admin.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 class YoutuberAdmin(admin.ModelAdmin):
     fieldsets = [
                  (None, {'fields': ['video_url', 'title', 'thumbnail_url']}),
-                 ('Date Information', {'fields': ['publish_start', 'publish_end']}),
-                 (u"""Advanced Data (don't change unless you know what you're doing!)""", {'fields': ['provider_url', 'author_name', 'author_url', 'video_code'], 'classes':['collapse']}),
+                 (u'Date Information', {'fields': ['publish_start', 'publish_end']}),
+                 (u'''Advanced Data (don't change unless you know what you're doing!)''', {'fields': ['provider_url', 'author_name', 'author_url', 'video_code'], 'classes':['collapse']}),
                  ]
     list_display = ('title', 'publish_start', 'publish_end', 'is_active', 'was_published_recently')
     list_filter = ['publish_start']
